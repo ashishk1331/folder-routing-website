48a53bce713001b7a11b264b9e53d4dc2443379f
Ashish Khare <akstyles211@gmail.com>
2024-02-09 21:35:27 +0530

    Update points and version

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function (props) {
+	return (
+		<footer className="w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 mx-auto">
+			<div className="text-gray-500 text-center mt-3">
+				© AshishK1331. All rights reserved.
+			</div>
+		</footer>
+	);
+}
